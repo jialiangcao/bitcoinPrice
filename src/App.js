eaa627fd6798ea827368aeaac3ad6d91fc1d336b
Matt <jcao2080@gmail.com>
2023-09-04 20:16:42 -0400

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const date = new Date();
+const formattedDate = date.toDateString();
+
 function App() {
+  const [bitcoinPrice, setBitcoinPrice] = useState(null);
+  const apiKey = "C241E351-9261-4911-81EE-64E669AF8019";
+  /* Insert coinapi key */
+
+  useEffect(() => {
+    async function fetchBitcoinPrice() {
+      try {
+        const response = await axios.get(`https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=${apiKey}`);
+        const { rate } = response.data;
+
+        const formattedBitcoinPrice = new Intl.NumberFormat('en', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        }).format(rate);
+
+        setBitcoinPrice(formattedBitcoinPrice);
+      } catch (error) {
+        console.error('Error fetching Bitcoin price:', error);
+      }
+    }
+
+    fetchBitcoinPrice();
+  }, [apiKey]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+        <h1>
+          What is the price of bitcoin?
+       </h1>
+       {bitcoinPrice !== null ? (
+          <h1>
+            On {formattedDate}, the price of bitcoin is: ${bitcoinPrice}
+         </h1>
+        ) : (
+          <p>Loading Bitcoin price...</p>
+        )}
+  </div>
   );
 }
 
